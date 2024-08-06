636038bace2cb6976cca8468ef3540191ae84664
Deepak Malik <125576282+Nishant9813@users.noreply.github.com>
2024-08-06 21:44:46 +0530

    make some better ui of navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/shareable components/Navbar";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import Browse from "./pages/Browse";
+
+
 function App() {
   return (
-    <>
-     <Navbar/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/browse" element={<Browse />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/shareable components/Navbar.jsx
+++ frontend/src/components/shareable components/Navbar.jsx
@@ -1,43 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { AiOutlineLogout } from "react-icons/ai";
+import { Button } from "../ui/button";
+import { CgProfile } from "react-icons/cg";
+import { TiThMenu } from "react-icons/ti";
+
 const Navbar = () => {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  const togglePopover = () => setPopoverOpen(!popoverOpen);
+
   return (
     <div className="border-2 border-black w-screen mx-auto">
       <div className="w-full flex items-center justify-between px-6 py-4 bg-black text-white">
-        <div className="flex items-center space-x-2">
-          <img src={logo} alt="logo" className="h-12 w-12" />
-          <p className="text-xl font-bold">
-            F <span className="text-red-600">Portal</span>
-          </p>
+        <div className="md:hidden">
+          <Drawer>
+            <DrawerTrigger>
+              <Button variant="none"><TiThMenu className="size-5"/></Button>
+            </DrawerTrigger>
+            <DrawerContent className="w-64 h-full bg-white ">
+              <DrawerHeader>
+                <DrawerTitle>Navigation Menu</DrawerTitle>
+                <DrawerDescription>
+                  Select an option from the menu.
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
         </div>
-        <div className="hidden xl:flex space-x-3">
-        {/* <NavigationMenu>
-  <NavigationMenuList>
-  <NavigationMenuItem>
-  <Link href="/docs" legacyBehavior passHref>
-    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-      Documentation
-    </NavigationMenuLink>
-  </Link>
-        </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu> */}
-<p>Hoem</p>
-<p>Hoem</p>
-<p>Hoem</p>
+        <div className="flex items-center space-x-2 cursor-pointer">
+          <Link to="/">
+            <img src={logo} alt="logo" className="h-12 w-12" />
+          </Link>
+          <Link to="/">
+            <p className="text-xl font-bold hidden md:block" >
+              F <span className="text-red-600">Portal</span>
+            </p>
+          </Link>
         </div>
-        <div className="flex flex-col justify-center items-center  ">
-          <Popover>
+        <div className="hidden md:flex space-x-5">
+          <Link to="/">Home</Link>
+          <Link to="/jobs">Jobs</Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger>Browse</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link to="/profile">Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link to="/billing">Billing</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
             <PopoverTrigger>
               <Avatar>
                 <AvatarImage src="https://github.com/shadcn.png" />
                 <AvatarFallback>Profile</AvatarFallback>
               </Avatar>
             </PopoverTrigger>
-            <PopoverContent>Place content for the popover here.</PopoverContent>
+            <PopoverContent className="w-64">
+              <div className="">
+                <div className="flex items-center space-x-4 mb-4">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>Profile</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <h4>Your Name</h4>
+                    <p className="text-sm text-muted-foreground">
+                      Full Stack Web Developer
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col items-start">
+                  <Link to="/jobs" onClick={togglePopover}>
+                    <Button variant="link">
+                      <CgProfile className="m-3" />
+                      View Profile
+                    </Button>
+                  </Link>
+                  <Link to="/browse" onClick={togglePopover}>
+                    <Button variant="link">
+                      <AiOutlineLogout className="m-3" />
+                      Logout
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </PopoverContent>
           </Popover>
         </div>
       </div>
